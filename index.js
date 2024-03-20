da98dce2ff7fe4080b3b1d3a28f314b1415ba72b
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-03-20 19:39:37 +0300

    complete io2lca

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const thankyous = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(thankyous) {
+    return thankyous.map(thankyous => `Thank you, ${thankyous}, for the wonderful surprise gift!`)
+} // .map calls back to the elements in the array
+
+const countDown = (number) => {
+    console.log(number);
+    if (number === 0) {
+      return; //returns back to the call stack, no # is returned
+    } 
+    else {
+      countDown(number - 1); // keeps counting back until it reaches 0
+    }
+  }
+countDown();
